242a3535951e3863bf4e1acb3cc7372c1dbb8087
Simon McEnlly <simon.mcenlly@gmail.com>
2021-02-19 08:31:29 +1000

    Change test to align with issue

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import {render, screen} from '@testing-library/react';
+import {act} from 'react-dom/test-utils';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Troubleshooting tests", () => {
+    it("it can render as expected", () => {
+        let dropdownLabel = "label for the dropdown";
+        act(() => {
+            render(<>
+                <label htmlFor="dropdown01">
+                    {dropdownLabel}
+                </label>
+                <select id="dropdown01">
+                    <option id="dropDownOption-1-0" value="">
+                        Option 01
+                    </option>
+                    <option id="dropDownOption-1-1" value="foo">
+                        Option 02
+                    </option>
+                </select>
+            </>)
+        });
+
+        let select = screen.getByLabelText(dropdownLabel) as HTMLSelectElement;
+        expect(select).toBeTruthy();
+        expect(select.item(0)?.textContent).toBe("Option 01");
+        let selectOption = select.item(0) as HTMLOptionElement;
+        expect(selectOption).toBeTruthy();
+        expect(selectOption.label).toBe("Option 01");
+    });
 });
